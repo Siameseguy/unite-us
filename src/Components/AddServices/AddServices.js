111b22402ffe3029243b5128f0aa26afc1b2e824
siameseguy <psyhakhom@gmail.com>
2019-05-01 17:14:34 -0400

    working on retrieving items from api

---
+++ src/Components/AddServices/AddServices.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class AddServices extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      error: null,
+      items: []
+    }
+  }
+
+  // async componentDidMount() {   try {     const response = await
+  // fetch('http://localhost:49567/api/service-types');     const json = await
+  // response.json();     this.setState({items: json});   } catch (error) {
+  // this.setState({error: 'this is an error'});   } }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:49567/api/service-types')
+      .then(res => this.setState({items: res.data}))
+      .catch(error => {
+        console.log(error);
+      })
+
+  }
+
+  render() {
+    return (
+      <div className="container">
+        {this.state.items && this
+          .state
+          .items
+          .map(item => (
+            <p key={item.id}>something</p>
+          ))}
+      </div>
+    )
+  }
+}
+
+export default AddServices;
